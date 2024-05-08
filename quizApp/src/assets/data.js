90c920b4f4b8d1f704f4610e97c8117d90acf6d4
khushikumari05 <khushigupta79kk@gmail.com>
2024-05-08 16:57:00 +0530

    first commit

---
+++ quizApp/src/assets/data.js
@@ -0,0 +1,46 @@
+export const data =[
+    {
+        question: "Identify the full form of HTTP?",
+        option1: "HyperText Transfer Protocol",
+        option2: "HyperText Transfer Package",
+        option3: "Hyper Transfer Text Package",
+        option4: "Hyper Transfer Text Practice",
+        ans: 1,
+    },
+
+    {
+        question: "The full form of OSI is?",
+        option1: "Operating System interface",
+        option2: "Optical System interconnection",
+        option3: "Operating System Internet",
+        option4: "Open System interconnection",
+        ans: 4,
+    },
+
+    {
+        question: "What is the number of layers in the OSI model?",
+        option1: "2 layers",
+        option2: "4 layers",
+        option3: "7 layers",
+        option4: "9 layers",
+        ans: 3,
+    },
+
+    {
+        question: "What does TCP/IP stand for?",
+        option1: "Telephone control protocol / Internet Protocol",
+        option2: "Transmission control protocol / Internet Protocol",
+        option3: "Transmisssion control protocol / International Protocol",
+        option4: "None",
+        ans: 2,
+    },
+
+    {
+        question: "Identify the layer which is responsible for data translating.",
+        option1: "Network",
+        option2: "Datalink",
+        option3: "Presentation",
+        option4: "Application",
+        ans: 3,
+    },
+];
